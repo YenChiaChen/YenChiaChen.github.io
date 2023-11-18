a6df4525d29ccc98094c620ead145d16b374c9d9
Yen-Chia Chen <107033180+YenChiaChen@users.noreply.github.com>
2023-11-18 11:57:14 +0800

    Add DaisyUI Plugins

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <button className="btn">Button</button>
+  </>
   );
 }
 
